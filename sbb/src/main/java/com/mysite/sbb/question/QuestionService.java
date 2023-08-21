26f742cddcffabbcf26a1df7508b490149b5f6ec
kosmo <kosmo@DESKTOP-88EGQ93>
2023-08-21 18:32:05 +0900

    yes

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
@@ -26,6 +29,12 @@ public class QuestionService {
 		} else {
 			throw new DataNotFoundException("question not found");
 		}
+	}
+	
+	// 페이징
+	public Page<Question> getList(int page) {
+		Pageable pageable = PageRequest.of(page, 10);
+		return this.questionRepository.findAll(pageable);
 	}
 	
 	// 질문 저장
