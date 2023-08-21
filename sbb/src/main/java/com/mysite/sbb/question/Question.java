de21e90f0af6b1fa9b992b4fb7b2e41886a2fe1c
kosmo <kosmo@DESKTOP-88EGQ93>
2023-08-21 12:41:04 +0900

    simple

---
+++ sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -0,0 +1,36 @@
+package com.mysite.sbb.question;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import com.mysite.sbb.answer.Answer;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+	@Id   // 기본키
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 1씩 자동증가
+	private Integer id;
+	
+	@Column(length = 200)
+	private String subject;
+	
+	@Column(columnDefinition = "TEXT")
+	private String content;
+	
+	private LocalDateTime createDate;
+	
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;
+}
