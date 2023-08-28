01a4a12ec77edaf23ec20b831ec2b3fe28a1b0a1
kosmo <kosmo@DESKTOP-88EGQ93>
2023-08-28 19:36:10 +0900

    2023-08-28 수정

--- sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
+++ sbb/src/main/java/com/mysite/sbb/question/QuestionService.java
@@ -12,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.mysite.sbb.DataNotFoundException;
+import com.mysite.sbb.user.SiteUser;
 
 import lombok.RequiredArgsConstructor;
 
@@ -42,11 +43,23 @@ public class QuestionService {
 	}
 	
 	// 질문 저장
-	public void create(String subject, String content) {
+	public void create(String subject, String content, SiteUser user) {
 		Question q = new Question();
 		q.setSubject(subject);
 		q.setContent(content);
 		q.setCreateDate(LocalDateTime.now());
+		q.setAuthor(user);
 		this.questionRepository.save(q);
+	}
+	
+	public void modify(Question question, String subject, String content) {
+		question.setSubject(subject);
+		question.setContent(content);
+		question.setModifyDate(LocalDateTime.now());
+		this.questionRepository.save(question);
+	}
+	
+	public void delete(Question question) {
+		this.questionRepository.delete(question);
 	}
 }
---
+++ sbb/src/main/java/com/mysite/sbb/user/UserSecurityService.java
@@ -0,0 +1,48 @@
+package com.mysite.sbb.user;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class UserSecurityService implements UserDetailsService {
+	
+	private final UserRepository userRepository;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// 사용자 이름(username)으로 데이터베이스에서 사용자 정보를 찾습니다.
+		Optional<SiteUser> _siteUser = this.userRepository.findByusername(username);
+		
+		// 사용자 정보가 존재하지 않으면 예외를 던집니다.
+		if (_siteUser.isEmpty()) {
+			throw new UsernameNotFoundException("사용자를 찾을 수 없습니다.");
+		}
+		
+		// 데이터베이스에서 찾은 사용자 정보를 가져옵니다.
+		SiteUser siteUser = _siteUser.get();
+		
+		// 사용자의 권한(authorities)를 담을 리스트를 생성합니다.
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		
+		// 사용자 이름이 "admin"인 경우 ADMIN 권한을 부여합니다.
+		if ("admin".equals(username)) {
+			authorities.add(new SimpleGrantedAuthority(UserRole.ADMIN.getValue()));
+		}
+		
+		// Spring Security에서 제공하는 User 객체를 생성하여 반환합니다.
+		// User 객체는 UserDetails 인터페이스를 구현한 클래스로, 사용자 정보와 권한 정보를 담고 있습니다.
+		return new User(siteUser.getUsername(), siteUser.getPassword(), authorities);
+	}
+}
